e115a8e1abb8e79c3518f4ff6186c0631a0e4f45
Alireza <alireza.hamami@gmail.com>
2024-02-15 17:06:46 -0400

    changed

--- app.js
+++ app.js
@@ -6,23 +6,6 @@ app.get('/', (req, res) => {
     res.send("this is respond from server")
 })
 
-app.get('/person/:name', (req, res) => {
-    res.send(`Person: ${req.params.name}`)
-})
-
-app.get('/data', (req, res) => {
-    res.json({"name":'Mick', "height":'176'})
-})
-
-app.get('/html', (req, res) => {
-    // res.setHeader('Content-type','text/html')
-    res.send(`<html>
-    <body> 
-    <h1>Hello</h1>
-    </body>
-    </html>`)
-})
-
 
 app.get('/information', (req, res) => res.send("this is information !"))
 
