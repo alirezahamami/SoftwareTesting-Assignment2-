2fd14f489b87d9155eff12889d93e61e5f050a08
alirezahamami <99290530+alirezahamami@users.noreply.github.com>
2024-02-15 16:22:28 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("this is respond from server")
+
+
+    console.log(req.headers)
+    console.log(req.method)
+})
+
+app.get('/person/:name', (req, res) => {
+    res.send(`Person: ${req.params.name}`)
+})
+
+app.get('/data', (req, res) => {
+    res.json({"name":'Mick', "height":'176'})
+})
+
+app.get('/html', (req, res) => {
+    // res.setHeader('Content-type','text/html')
+    res.send(`<html>
+    <body> 
+    <h1>Hello</h1>
+    </body>
+    </html>`)
+})
+
+
+app.get('/information', (req, res) => res.send("this is information !"))
+
+app.listen(3000, () => console.log('listen is started on port 3000 !'))
