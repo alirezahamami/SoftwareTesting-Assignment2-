737d1faea858d7e84620f0aa3586e54f3dd14802
Alireza <alireza.hamami@gmail.com>
2024-02-15 17:42:28 -0400

    final

--- app.test.js
+++ app.test.js
@@ -1,29 +1,10 @@
 const request = require('supertest');
 const app = require('./app');
 
-// Define the server instance and port outside the test case
-let server;
-const PORT = 3001; // Use a different port for testing
-
-beforeAll((done) => {
-  // Start the Express server before running tests
-  server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    done();
-  });
-});
-
 describe('Test Express app root endpoint', () => {
   it('should respond with status code 200 and message "this is respond from server"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('this is respond from server');
   });
-});
-
-// Close the server instance after all tests are done
-afterAll((done) => {
-  if (server) {
-    server.close(done);
-  }
 });
